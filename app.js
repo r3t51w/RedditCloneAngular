37358cb1a2e029c0950144b2cf9c90c13e647a15
R3t51W <r3t51w@github.com>
2016-05-09 01:14:45 -0400

    added comments into prefill data

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ angular.module('redditClone',['ui.router'])
 .factory('posts', [function(){
   var o={
     posts: [
-      {title: "Post 1", upvotes: 10},
-      {title: "Post 2", upvotes: 7},
-      {title: "Post 3", upvotes: 9},
-      {title: "Post 4", upvotes: 11},
-      {title: "Post 5", upvotes: 6},
-      {title: "Post 6", upvotes: 3},
-      {title: "Post 7", upvotes: 5}
+      {title: "Post 1", upvotes: 10, comments:[]},
+      {title: "Post 2", upvotes: 7, comments:[]},
+      {title: "Post 3", upvotes: 9, comments:[]},
+      {title: "Post 4", upvotes: 11, comments:[]},
+      {title: "Post 5", upvotes: 6, comments:[]},
+      {title: "Post 6", upvotes: 3, comments:[]},
+      {title: "Post 7", upvotes: 5, comments:[]}
     ]
   };
   return o;
